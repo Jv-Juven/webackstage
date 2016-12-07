0c2a8fa1133d48a47fb8aaa98a3d75c415e80f55
wuhui.zhu <wuhui.zhu@platenogroup.com>
2016-12-07 18:28:43 +0800

    创建项目

---
+++ start.js
@@ -0,0 +1,8 @@
+// 参考：http://www.liaoxuefeng.com/wiki/001434446689867b27157e896e74d51a89c25cc8b43bdb3000/001471087582981d6c0ea265bf241b59a04fa6f61d767f6000
+var register = require('babel-core/register');
+
+register({
+	presets: ['stage-3']
+});
+
+require('./app.js');
